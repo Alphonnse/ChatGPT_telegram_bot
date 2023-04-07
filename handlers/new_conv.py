d6bd002ca78aba863f2542e4305d331b20ff9cfb
Alphonnse <nderzyan@gmail.com>
2023-04-07 12:19:28 +0300

    debug of history2

--- handlers/new_conv.py
+++ handlers/new_conv.py
@@ -44,6 +44,7 @@ async def start_conv(message: Message):
     user_id = message.from_user.id
     chat_id = message.chat.id
 
+    """Iterationg the conversations"""
     if user_id not in convs_count:
         convs_count[user_id] = 0
     else:
@@ -55,15 +56,15 @@ async def start_conv(message: Message):
                 break
         convs_count[user_id] = j
     
-    print(users)
-
+    """Creating the {users:{chat_id:[[],[],[],[],[]]}}"""
     if user_id not in users:
         chats = {}
         users[user_id] = chats
         if chat_id not in chats:
             conversations = [list((None, None)) for x in range(5)]
             chats[chat_id] = conversations
-
+    
+    """Initializing the user"""
     users[user_id][chat_id][convs_count[user_id]][1] = Chat()
     users[user_id][chat_id][convs_count[user_id]][1].get_api()
     users[user_id][chat_id][convs_count[user_id]][1].add_system()
@@ -73,7 +74,6 @@ async def start_conv(message: Message):
         "Вы начали новый диалог без контекста.",
         reply_markup=keyboard
     )
-    
 
     if user_id not in request_count:
         request_count[user_id] = 1
@@ -91,26 +91,29 @@ async def all_conv(message: Message):
     """the coverstaion history"""
     user_id = message.from_user.id
     chat_id = message.chat.id
-   
-    builder = ReplyKeyboardBuilder()
-    j = 0
-    while users[user_id][chat_id][j][0] is not None:
-        j += 1
-        if j == 5:
-            break
-    if j == 0:
-        # await message.answer("There are no conversations yet")
-        await message.answer("У вас пока нет диалогов")
-    else:
-        for i in range(j):
-            builder.add(types.KeyboardButton(text=users[user_id][chat_id][i][0]))
-        builder.adjust(2)
+    
+    if user_id in users:
+        builder = ReplyKeyboardBuilder()
+        j = 0
+        while users[user_id][chat_id][j][0] is not None:
+            j += 1
+            if j == 5:
+                break
+        if j == 0:
+            # await message.answer("There are no conversations yet")
+            await message.answer("У вас пока нет диалогов")
+        else:
+            for i in range(j):
+                builder.add(types.KeyboardButton(text=users[user_id][chat_id][i][0]))
+            builder.adjust(2)
 
-        await message.answer(
-               # "Select the conversation you want to return to.",
-               "Выберите диалог к которому вы хотите вернуться.",
-               reply_markup=builder.as_markup(resize_keyboard=True)
-                )
+            await message.answer(
+                # "Select the conversation you want to return to.",
+                "Выберите диалог к которому вы хотите вернуться.",
+                reply_markup=builder.as_markup(resize_keyboard=True)
+                    )
+    else:
+        await message.answer("У вас пока нет диалогов")
 
 
 @router.message(F.text)
